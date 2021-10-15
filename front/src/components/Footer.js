3ae9e91cb6cbc1418da7f10d4130c55232a01f69
Rst Rlm <risto.reilama@gmail.com>
2021-10-15 15:54:11 +0300

    modified footer and styles for it

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Footer = () => {
     return (
       <footer className="footer">
-          <p>This shall be my footer</p>
+          <section className="center">
+            <article className="some">
+              links to socialmedia
+            </article>
+            <article className="contact">
+            email: <a href="mailto:risto.reilama@gmail.com">risto.reilama@gmail.com</a> 
+            phone: <a href="tel:+358401743425">+358401743425</a>
+            </article>
+          
+          </section>
       </footer>
     )
   }
